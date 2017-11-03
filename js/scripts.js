3e1659e367ca732c1bef41833af848b54c7119d2
Shane Robinson <microbistro@gmail.com>
2017-11-03 16:32:22 -0700

    add the boop feature including all the teens.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $("form#number-beep").submit(function(event) {
     event.preventDefault();
-    var number = parseInt($("input#number").val());
 
+    var number = parseInt($("input#number").val());
     var newNumber = [];
 
     for(var index = 0; index <= number; index += 1) {
@@ -10,12 +10,15 @@ $(document).ready(function() {
       if (beepboop % 10 === 0 || beepboop === 0 ) {
         beepboop = "beep"
       }
-       else if (beepboop % 3 === 0 ) {
-         beepboop = "I'm sorry dave. I can not do that!"
-       }
-      newNumber.push(beepboop)
-    }
+        else if (beepboop % 10 === 1 || beepboop === 1 || (index > 10 && index < 20 )) {
+        beepboop= "boop"
+      }
+        else if (beepboop % 3 === 0 ) {
+        beepboop = "I'm sorry dave. I can not do that!"
+      }
+        newNumber.push("<li>" + beepboop + "</li>")
+      }
 
-    $("#result").text(newNumber);
+      $("#result").append(newNumber);
   });
 });
